2ee7f412a59a7f66e3e4e2e9fc88b286eb660460
havafy <contact@havafy.com>
2021-06-28 16:53:42 +0700

    setup GA and Chatra

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 const recaptchaScriptSource = `https://www.google.com/recaptcha/api.js?render=` + process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
-
+const ChatraID = process.env.NEXT_PUBLIC_CHATRA
+const GA_ID = process.env.NEXT_PUBLIC_GA
+const GA_URL = `https://www.googletagmanager.com/gtag/js?id=` + GA_ID
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -12,6 +14,29 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <script src={recaptchaScriptSource}></script>
+        <script async src={GA_URL}></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+              ` }} />
+          
+            <script dangerouslySetInnerHTML={{ __html: `
+              (function(d, w, c) {
+                      w.ChatraID = '${ChatraID}';
+                      var s = d.createElement('script');
+                      w[c] = w[c] || function() {
+                          (w[c].q = w[c].q || []).push(arguments);
+                      };
+                      s.async = true;
+                      s.src = 'https://call.chatra.io/chatra.js';
+                      if (d.head) d.head.appendChild(s);
+                  })(document, window, 'Chatra');
+              ` }} />
+
+
+
         </Head>
         <body className="layout">
           <Main />
