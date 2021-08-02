786d9e9355dbaa498204b96dc777999a50875470
havafy <contact@havafy.com>
2021-08-02 21:58:10 +0700

    add fb chat

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 const recaptchaScriptSource = `https://www.google.com/recaptcha/api.js?render=` + process.env.NEXT_PUBLIC_RECAPTCHA_KEY;
-const ChatraID = process.env.NEXT_PUBLIC_CHATRA
 const GA_ID = process.env.NEXT_PUBLIC_GA
 const GA_URL = `https://www.googletagmanager.com/gtag/js?id=` + GA_ID
 class MyDocument extends Document {
@@ -24,39 +23,39 @@ class MyDocument extends Document {
           gtag('config', '${GA_ID}');
               ` }} />
 
-
-            <div id="fb-root"></div>
-            <div id="fb-customer-chat" class="fb-customerchat">
-            </div>
-            <script dangerouslySetInnerHTML={{ __html: `
-              var chatbox = document.getElementById('fb-customer-chat');
-              chatbox.setAttribute("page_id", "105109347625874");
-              chatbox.setAttribute("attribution", "biz_inbox");
-
-              window.fbAsyncInit = function() {
-                FB.init({
-                  xfbml            : true,
-                  version          : 'v11.0'
-                });
-              };
-
-              (function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-                fjs.parentNode.insertBefore(js, fjs);
-              }(document, 'script', 'facebook-jssdk'));
-
-              ` }} />
-
-
-
         </Head>
         <body className="layout">
           <Main />
           <NextScript />
         </body>
+
+
+        <div id="fb-root"></div>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v11.0'
+              });
+            };
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+            `,
+          }}
+        />
+
+        <div className="fb-customerchat"
+          attribution="page_inbox"
+          page_id="105109347625874">
+        </div>
+        
       </Html>
     )
   }
